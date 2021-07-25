9c330b2e9e02b56f6106de1ede1ec87d12d8775d
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2021-07-25 22:53:09 +0200

    Update square.py

--- sources/square.py
+++ sources/square.py
@@ -4,10 +4,10 @@ from sources.ploter.plot_square_tiling import square_tiling
 
 if __name__ == "__main__":
     color = {0: "blue", 1: "red"}
-    checkerboard = np.zeros((10, 10), dtype=int)
+    square = np.zeros((10, 10), dtype=int)
 
-    for i in range(len(checkerboard)):
-        for j in range(len(checkerboard[i])):
-            checkerboard[i][j] = i % 2 == j % 2
-    print(checkerboard)
-    square_tiling(checkerboard, "test.png", color, 10)
+    for i in range(len(square)):
+        for j in range(len(square[i])):
+            square[i][j] = i % 2 == j % 2
+    print(square)
+    square_tiling(square, "test.png", color, 10)
